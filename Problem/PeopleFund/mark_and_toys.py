e405a9449092703f8cd487dfa47a9552e205ef34
Yeojin Kim <yeojin.dev@gmail.com>
2018-11-12 07:01:18 +0900

    PeopleFund-025: Mark and Toys

---
+++ Problem/PeopleFund/mark_and_toys.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    prices.sort()
+    sum_prices = 0
+    count = 0
+    for price in prices:
+        sum_prices += price
+        if sum_prices > k:
+            break
+        count += 1
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
