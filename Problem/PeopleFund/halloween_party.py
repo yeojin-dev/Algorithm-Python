f57099c9ab700d290a75d35bcc63cc44a0828382
Yeojin Kim <yeojin.dev@gmail.com>
2019-04-23 23:17:42 +0900

    PeopleFund-099: Halloween Party

---
+++ Problem/PeopleFund/halloween_party.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the halloweenParty function below.
+#
+def halloweenParty(k):
+    return (k // 2) * (k - (k // 2))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        k = int(input())
+
+        result = halloweenParty(k)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
