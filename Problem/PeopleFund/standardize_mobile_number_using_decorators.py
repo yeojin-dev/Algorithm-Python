1cae206a50f0235c94f5edb179281e06b911473a
Yeojin Kim <yeojin.dev@gmail.com>
2019-08-11 23:36:23 +0900

    PeopleFund-127: Standardize Mobile Number Using Decorators

---
+++ Problem/PeopleFund/standardize_mobile_number_using_decorators.py
@@ -0,0 +1,11 @@
+def wrapper(f):
+    def fun(l):
+        f([f"+91 {number[-10:-5]} {number[-5:]}" for number in l])
+    return fun
+
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+if __name__ == '__main__':
+    l = [input() for _ in range(int(input()))]
