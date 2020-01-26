6595c432ac718267162bd41986b72e958869623b
Yeojin Kim <yeojin.dev@gmail.com>
2020-01-27 08:42:47 +0900

    PeopleFund-172: Left Rotation

---
+++ Problem/PeopleFund/left_rotation.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    r = d % n
+
+    l = a[r:] + a[:r]
+
+    print(*l)
